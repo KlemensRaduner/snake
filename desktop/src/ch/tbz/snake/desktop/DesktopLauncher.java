54e9ab0b4b66890ca2e836d49c77907f10bf99dc
klemens <klemens.raduner@nyp.ch>
2020-06-11 11:08:42 +0200

    project added

---
+++ desktop/src/ch/tbz/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.tbz.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.tbz.snake.SnakeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnakeGame(), config);
+	}
+}
