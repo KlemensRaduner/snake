6637fa1b5636e5d30cae19e0e0bfe284273ec9a2
klemens <klemens.raduner@nyp.ch>
2020-06-11 11:27:28 +0200

    set debug level

--- core/src/ch/tbz/snake/SnakeGame.java
+++ core/src/ch/tbz/snake/SnakeGame.java
@@ -1,12 +1,16 @@
 package ch.tbz.snake;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class SnakeGame extends Game {
 
 	MainMenuScreen mainMenuScreen;
 	@Override
 	public void create () {
+
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 
 		mainMenuScreen = new MainMenuScreen(this);
 
